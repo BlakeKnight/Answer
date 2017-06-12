79db13d562597f8d2ce09cc67df9ee47743e2a2c
Arthur Knight <theblackmib@gmail.com>
2017-06-12 18:37:43 -0400

    sample

---
+++ cheetahcopy.rb
@@ -0,0 +1,55 @@
+beatles = [
+  {
+    name: nil,
+    nickname: "The Smart One"
+  },
+  {
+    name: nil,
+    nickname: "The Funny One"
+  },
+  {
+    name: nil,
+    nickname: "The Cute One"
+    },
+  {
+    name: nil,
+    nickname: "The Quiet One"
+  }
+]
+
+i = 0
+while i < beatles.length
+  case beatles[i][:nickname]
+  when "The Smart One"
+    beatles[i][:name] = "John"
+  when "The Funny One"
+    beatles[i][:name] = "Ringo"
+  when "The Cute One"
+    beatles[i][:name] = "Paul"
+  when "The Quiet One"
+    beatles[i][:name] = "George"
+  end
+  i += 1
+end
+
+i = 0
+while i < beatles.length
+  puts "Hi, I'm #{beatles[i][:name]}.  I'm #{beatles[i][:nickname]}!"
+  i += 1
+end
+
+
+# efficient
+
+beatles.each do |beatle|
+  beatle[:name] = case beatle[:nickname]
+                  when "The Smart One"
+                    "John"
+                  when "The Funny One"
+                    "Ringo"
+                  when "The Cute One"
+                    "Paul"
+                  when "The Quiet One"
+                     "George"
+                  end
+end
